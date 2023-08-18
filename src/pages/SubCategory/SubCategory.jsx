2049bfb46540757f1041ead7d30a0811014a3225
nikhilroy <nikhilchandraroypoet@gmail.com>
2023-08-18 13:19:38 +0600

    category page done

---
+++ src/pages/SubCategory/SubCategory.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Nominators from '../Awards/Nominators';
+import ProductInfo from '../../components/ProductInfo';
+function SubCategory(props) {
+    const [isLargeText, setIsLargeText] = useState(false)
+    return (
+        <div id='SubCategory'>
+            <div className="h-full bg-backdrop">
+                <div className="h-full min-h-[calc(100vh-64px)] bg-[#fcfcfc] rounded-t-lg relative md:mx-2 px-3 pt-8 flex relative justify-center gap-x-12">
+                    {/* item */}
+
+                    <div className="w-full max-w-3xl">
+                        <header className="mt-5 mb-12">
+                            <h5 className="text-h2">Centralized Exchanges (CEX)</h5>
+                            <h2 className="text-b1 font-semibold text-foreground-onEmphasis mt-6 mb-3">What is "Centralized Exchanges (CEX)"?</h2>
+                            <div className="text-b2 flex flex-col gap-6">
+                                <p>Centralized Exchanges are platforms where users can trade cryptocurrencies through a centralized intermediary. These exchanges act as trusted third parties that facilitate transactions, provide order matching, and ensure liquidity. Centralized Exchanges often offer advanced trading features,{isLargeText ? ' such as margin trading and order types, and provide user-friendly interfaces for easy trading.' : '...'}
+                                </p>
+                            </div>
+
+                            <button onClick={() => setIsLargeText(!isLargeText)} className="text-foreground-onEmphasis !border-none underline mt-3 hover:text-opacity-80 text-b2">
+                                Show {isLargeText ? 'less' : 'more'}
+                            </button>
+                        </header>
+
+                        {/* ProductInfo */}
+                        <ProductInfo></ProductInfo>
+                        {/* ProductInfo End */}
+                    </div>
+                    {/* item */}
+                    <Nominators></Nominators>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SubCategory;
--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -9,6 +9,7 @@ import Awards from "../pages/Awards/Awards";
 import Apply from "../pages/Apply/Apply";
 import Profile from "../pages/Profile/Profile";
 import MyProducts from "../pages/MyProducts/MyProducts";
+import SubCategory from "../pages/SubCategory/SubCategory";
 // .................Pages............................End
 
 function MyRoutes() {
@@ -21,6 +22,7 @@ function MyRoutes() {
                     <Route path="/users/:username" element={<Profile></Profile>}></Route>
                     <Route path="/awards" element={<Awards></Awards>}></Route>
                     <Route path="/my-products" element={<MyProducts></MyProducts>}></Route>
+                    <Route path="/subcategory/:category_name" element={<SubCategory></SubCategory>}></Route>
                 </Routes>
             </Base>
         </BrowserRouter>
